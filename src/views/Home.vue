a059b60f5f5d4fa17d3babfba1b6a2886c6f0e4b
deepak <deepamdeebu@gmail.com>
2024-02-20 14:38:51 +0530

    Fetch data : Fetch data in home page after json server . Also rename file

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+        <p>{{ task.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.tasks = data;
+      })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
